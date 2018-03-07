43b55edb566cef1edf6d066d8dffb15d6ac23f89
yuliang <yu.liang@freelog.com>
2018-03-07 11:01:53 +0800

    1.修改配置

--- config/config.test.js
+++ config/config.test.js
@@ -1,7 +1,25 @@
 'use strict'
 
-module.exports = appInfo => {
-    return {
-        gatewayUrl: "http://192.168.164.129",
-    }
+module.exports = {
+    /**
+     * DB-mysql相关配置
+     */
+    knex: {
+        node: {
+            connection: {
+                host: 'rm-wz9wj9435a0428942.mysql.rds.aliyuncs.com',
+                user: 'freelog',
+                password: 'Ff@233109',
+                database: 'fr_node'
+            },
+            debug: false
+        }
+    },
+
+    /**
+     * api网关内网地址
+     */
+    //gatewayUrl: "http://172.18.215.224:8895",
+
+    nodeHomePageTemplateUrl: "http://frcdn.oss-cn-shenzhen.aliyuncs.com/web-components/index.html"
 }
