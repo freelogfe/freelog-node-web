95e9798488e3ee9b386d9849a9c729e82d7684d9
yuliang <yu.liang@freelog.com>
2018-12-27 10:57:25 +0800

    1.更新配置版本

--- config/config.test.js
+++ config/config.test.js
@@ -5,8 +5,8 @@ module.exports = {
     cluster: {
         listen: {port: 5777}
     },
-    
-    gatewayUrl: "http://172.18.215.224:8895/test",
+
+    gatewayUrl: "http://172.18.215.229:6895/test",
 
     nodeHomePageTemplateUrl: "http://test-frcdn.oss-cn-shenzhen.aliyuncs.com/pages/pagebuild.html"
 }
--- config/plugin.js
+++ config/plugin.js
@@ -1,10 +1,5 @@
 'use strict';
 
-exports.freelogDataBase = {
-    enable: true,
-    package: 'egg-freelog-database',
-};
-
 exports.alinode = {
     enable: true,
     package: 'egg-alinode',
