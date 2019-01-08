d0e5dcabe9b5eb96caf704066c903b2317d03797
yuliang <yu.liang@freelog.com>
2019-01-08 10:58:03 +0800

    1.优化代码

--- app/controller/home.js
+++ app/controller/home.js
@@ -16,15 +16,11 @@ module.exports = class HomeController extends Controller {
 
         const presentableAuthUrl = `${ctx.webApi.authInfo}/presentable/${nodeInfo.pageBuildId}.data?nodeId=${nodeInfo.nodeId}`
         await ctx.curlIntranetApi(presentableAuthUrl, {dataType: 'original'}).then(response => {
-
-            const [contentTypeKey, authTokenKey, subResourceIdsKey] = this._findKeyIgnoreUpperLower(response.res.headers, "content-type", "freelog-sub-resource-auth-token", "freelog-sub-resourceIds")
-            if (response.res.headers[contentTypeKey].includes('application/json')) {
+            const [contentType, widgetToken, subResourceIds] = this._findValueByKeyIgnoreUpperLower(response.res.headers, "content-type", "freelog-sub-resource-auth-token", "freelog-sub-resourceIds")
+            if (contentType.includes('application/json')) {
                 this._pageBuildAuthFailedHandle(JSON.parse(response.data.toString()), nodeInfo, userId)
                 return
             }
-            
-            const widgetToken = response.res.headers[authTokenKey]
-            const subResourceIds = response.res.headers[subResourceIdsKey]
             ctx.body = ctx.helper.nodeTemplateHelper.convertNodePageBuild(ctx.config.nodeTemplate, response.data.toString(), nodeInfo, userId, widgetToken, subResourceIds)
         })
     }
@@ -49,9 +45,12 @@ module.exports = class HomeController extends Controller {
      * 忽略大小写查找对象的key,返回第一个
      * @private
      */
-    _findKeyIgnoreUpperLower(object, ...args) {
+    _findValueByKeyIgnoreUpperLower(object, ...args) {
         const keys = Object.keys(object)
-        const result = args.map(key => keys.find(x => x.toLowerCase() === key.toLowerCase()))
+        const result = args.map(key => {
+            const realKey = keys.find(x => x.toLowerCase() === key.toLowerCase())
+            return object[realKey]
+        })
         return args.length === 1 ? result[0] : result
     }
 
